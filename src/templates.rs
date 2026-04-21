7a9bd69d0c21346f3a5e18c96f34beb4b9099372
ivanantipov2312 <ivanantipov231@gmail.com>
2026-04-21 09:43:56 +0300

    switch from lazy_static to rust standard alternative

--- src/templates.rs
+++ src/templates.rs
@@ -1,18 +1,15 @@
+use std::sync::LazyLock;
 use tera::Tera;
-use lazy_static::lazy_static;
 
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/**/*.html") {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Error parsing templates! Error: {e}");
-                ::std::process::exit(1);
-            }
-        };
-
-        tera.autoescape_on(vec![".html", ".sql"]);
-        tera
+pub static TEMPLATES: LazyLock<Tera> = LazyLock::new(|| {
+    let mut tera = match Tera::new("templates/**/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Error parsing templates! Error: {e}");
+            ::std::process::exit(1);
+        }
     };
-}
 
+    tera.autoescape_on(vec![".html", ".sql"]);
+    tera
+});
